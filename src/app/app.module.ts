6270c1edbb2b6de7eef85f98478a65f5b0d15b96
Omar Mohamed <121135630+OmarGeroy@users.noreply.github.com>
2023-03-12 20:48:08 +0200

    creating project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TripsComponent } from './omarCom/trips/trips.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TripsComponent
   ],
   imports: [
     BrowserModule,
